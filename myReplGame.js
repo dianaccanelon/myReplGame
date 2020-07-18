555d1e71735736bb5f0b0fc8b43f3bd69b051b38
dianaccanelon <dianaccanelon@gmail.com>
2020-07-18 17:08:07 -0400

    add start script

---
+++ myReplGame.js
@@ -0,0 +1,35 @@
+const readlineSync = require("readline-sync");
+
+//Welcome the user - It's Friday night, Barbie & Ken are going on their 4th date <3
+//Barbie is freaking out! What should she wear? What if she doesn't like the 
+//restaurant Ken picks? Will they have their first kiss? So much can happen tonight! Can you help Barbie get ready for her date?
+
+//Barbie: "First thing's first what should I wear tonight?" "I have a cute blue mini dress that compliments my eyes, these leather leggings with a tanktop and a leather jacket, or I can wear a pair of jeans with a cute blazer."
+//Present choices for selection
+
+//Barbie: "Ok! I will wear the DRESS-CHOICE! I'll go get dressed and finish getting ready. I have to hurry! Ken will be here any minute."
+
+//KEN ARRIVES. DING-DONG!
+//Barbie: "He's here!" Barbie opens the front door and greets Ken. 
+//Ken: "Hi Barbie! It's nice to see you. Ready to go?"
+
+//KEN AND BARBIE HEAD OVER TO SOKAI, A JAPAENESE-PREUVIAN FUSION RESTAURANT. YUM!
+
+//Barbie thinks to herself: "Hmm... What should I have?"
+//Ready to order? The menu has a number of choices. Barbie is eyeing the sushi pizza, the beef tenderloin stir fry and the salmon tacos. Which one do you think she should choose?
+//Present choices for selection
+
+//BARBIE AND KEN ARE CHATTING, KEN ASKS BARBIE TO BE HIS GIRLFRIEND
+//Ken: Barbie, I have had a great time with you over these last few dates, I would really like to make you a part of my life. Will you be my girlfriend.
+//What should Barbie do?
+//Present choices for selection: 1) YES! 2) I think its too soon Ken, is it ok if we get to know each other more? 3)No, I was really just looking to have fun.
+
+
+//DINNER ENDS, KEN DRIVES BARBIE HOME AND WALKS HER TO HER DOOR
+//Scenarios: If Barbie says yes: Ken says "Barbie, I had so much fun tonight. See you tomorrow, girlfriend." and leans in for a kiss. 
+//If Barbie said it's too soon, Ken says "Barbie, I had so much fun tonight. I hope you did too. I'll call you tomorrow to setup another date?"
+//If Barbie said no, Ken says: "I undestand you were only looking for fun Barbie, but I am looking for a relationship, it might be best for us to just be friends, I'll see you around."
+
+
+console.log("Hi!")
+
