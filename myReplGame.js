1a84408eac112b64900160b4ce35ef0c80bad17c
dianaccanelon <dianaccanelon@gmail.com>
2020-07-18 18:59:58 -0400

    text done, missing question prompts

--- myReplGame.js
+++ myReplGame.js
@@ -1,5 +1,7 @@
 const readlineSync = require("readline-sync");
 
+//STORYLINE:
+
 //Welcome the user - It's Friday night, Barbie & Ken are going on their 4th date <3
 //Barbie is freaking out! What should she wear? What if she doesn't like the 
 //restaurant Ken picks? Will they have their first kiss? So much can happen tonight! Can you help Barbie get ready for her date?
@@ -11,16 +13,16 @@ const readlineSync = require("readline-sync");
 
 //KEN ARRIVES. DING-DONG!
 //Barbie: "He's here!" Barbie opens the front door and greets Ken. 
-//Ken: "Hi Barbie! It's nice to see you. Ready to go?"
+//Ken: "Hi Barbie! Wow! You look amazing. Ready to go?"
 
 //KEN AND BARBIE HEAD OVER TO SOKAI, A JAPAENESE-PREUVIAN FUSION RESTAURANT. YUM!
 
 //Barbie thinks to herself: "Hmm... What should I have?"
-//Ready to order? The menu has a number of choices. Barbie is eyeing the sushi pizza, the beef tenderloin stir fry and the salmon tacos. Which one do you think she should choose?
+//The menu has a number of choices. Barbie is eyeing the sushi pizza, the beef tenderloin stir fry and the salmon tacos. Which one do you think she should choose?
 //Present choices for selection
 
 //BARBIE AND KEN ARE CHATTING, KEN ASKS BARBIE TO BE HIS GIRLFRIEND
-//Ken: Barbie, I have had a great time with you over these last few dates, I would really like to make you a part of my life. Will you be my girlfriend.
+//Ken: I am stuffed! Did you like your BARBIES FOOD SELECTION?Barbie, I have had a great time with you over these last few dates, I would really like to make you a part of my life. Will you be my girlfriend.
 //What should Barbie do?
 //Present choices for selection: 1) YES! 2) I think its too soon Ken, is it ok if we get to know each other more? 3)No, I was really just looking to have fun.
 
@@ -31,5 +33,67 @@ const readlineSync = require("readline-sync");
 //If Barbie said no, Ken says: "I undestand you were only looking for fun Barbie, but I am looking for a relationship, it might be best for us to just be friends, I'll see you around."
 
 
-console.log("Hi!")
+//INTRODUCTION
+
+console.log("It's Friday night, Barbie & Ken are going on their 4th date <3\n");
+
+console.log("Barbie is freaking out! What should she wear? What if she doesn't like the restaurant Ken picks? Will they have their first kiss?\nSo much can happen tonight! Can you help Barbie get ready for her date?\n");
+
+console.log("Barbie: \"First things first what should I wear tonight? I have:\n A) A cute blue mini dress that compliments my eyes\n B) These leather leggings with a tanktop and a leather jacket\n C) A pair of jeans with a cute blazer.\"");
+
+//QUESTION 1
+console.log("\nWhat should barbie wear?\n");
+
+//QUESTION 1 - CONFIRMATION - ELSEIF
+console.log(`Barbie: \"Ok! I will wear the DRESS-CHOICE! I'll go get dressed and finish getting ready. I have to hurry! Ken will be here any minute.\"`);
+
+console.log("\nKEN ARRIVES. DING-DONG!\n");
+
+console.log("Barbie: \"He's here!\" Barbie opens the front door and greets Ken.");
+console.log("Ken: \"Hi Barbie! Wow! You look amazing. Ready to go?\"\n");
+
+console.log("KEN AND BARBIE HEAD OVER TO SOKAI, A JAPAENESE-PREUVIAN FUSION RESTAURANT. YUM!");
+
+console.log("\nBarbie thinks to herself: \"Hmm... What should I have?\"");
+
+//QUESTION 2
+console.log("\nThe menu has a number of choices. Barbie is eyeing the sushi pizza, the beef tenderloin stir fry and the salmon tacos.\n\nWhich one do you think she should choose?\n A) Sushi Pizza\n B) Beef Tenderloin Stir Fry\n C) Salmon Tacos")
+
+console.log("\nBARBIE AND KEN ARE CHATTING, KEN ASKS BARBIE TO BE HIS GIRLFRIEND\n");
+
+//QUESTION 2 - CONFIRMATION - ELSEIF
+
+console.log("Ken: I am stuffed! Did you like your BARBIES FOOD SELECTION?\"");
+
+console.log("Barbie: \"It was absolutely delicious\"");
+
+console.log("Ken: \"Barbie, I have had a great time with you over these last few dates, I would really like to make you a part of my life. Will you be my girlfriend?\"");
+
+//QUESTION 3
+
+console.log("What should Barbie say?\n A) YES!\n B) I think its too soon Ken, is it ok if we get to know each other more?\n C) No, I was really just looking to have fun.\n")
+
+console.log("DINNER ENDS, KEN DRIVES BARBIE HOME AND WALKS HER TO HER DOOR")
+
+
+//Scenarios: 
+//If Barbie says yes: Ken says "Barbie, I had so much fun tonight. See you tomorrow, girlfriend." and leans in for a kiss. 
+//If Barbie said it's too soon, Ken says "Barbie, I had so much fun tonight. I hope you did too. I'll call you tomorrow to setup another date?"
+//If Barbie said no, Ken says: "I undestand you were only looking for fun Barbie, but I am looking for a relationship, it might be best for us to just be friends, I'll see you around."
+
+
+console.log("Barbie: \"Thanks for helping me tonight my friend!\"")
+
+
+
+
+
+
+
+
+
+
+
+
+
 
