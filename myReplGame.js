2dcca570bdf2f61dea499d0d05792077162c8419
dianaccanelon <dianaccanelon@gmail.com>
2020-07-19 10:28:46 -0400

    almost there

--- myReplGame.js
+++ myReplGame.js
@@ -1,5 +1,9 @@
 const readlineSync = require("readline-sync");
 
+readlineSync.setDefaultOptions({limit: ['a', 'b', 'c', 'A', 'B', 'C']});
+
+
+
 //STORYLINE:
 
 //Welcome the user - It's Friday night, Barbie & Ken are going on their 4th date <3
@@ -35,17 +39,27 @@ const readlineSync = require("readline-sync");
 
 //INTRODUCTION
 
-console.log("It's Friday night, Barbie & Ken are going on their 4th date <3\n");
+console.log("\n\nIt's Friday night, Barbie & Ken are going on their 4th date <3\n");
 
 console.log("Barbie is freaking out! What should she wear? What if she doesn't like the restaurant Ken picks? Will they have their first kiss?\nSo much can happen tonight! Can you help Barbie get ready for her date?\n");
 
-console.log("Barbie: \"First things first what should I wear tonight? I have:\n A) A cute blue mini dress that compliments my eyes\n B) These leather leggings with a tanktop and a leather jacket\n C) A pair of jeans with a cute blazer.\"");
+console.log("Barbie: \"First things first what should I wear tonight? I have:\n a) A cute blue mini dress that compliments my eyes\n b) These leather leggings with a tanktop and a leather jacket\n c) A pair of jeans with a cute blazer.\"");
 
 //QUESTION 1
-console.log("\nWhat should barbie wear?\n");
+let outfit = readlineSync.keyIn("\nWhat should barbie wear?\n",  {limit: 'abcABC'})
 
 //QUESTION 1 - CONFIRMATION - ELSEIF
-console.log(`Barbie: \"Ok! I will wear the DRESS-CHOICE! I'll go get dressed and finish getting ready. I have to hurry! Ken will be here any minute.\"`);
+
+
+if(outfit == 'a' || outfit == 'A'){
+    console.log(`Barbie: \"Ok! I will wear the cute blue mini! I'll go get dressed and finish getting ready. I have to hurry! Ken will be here any minute.\"`);
+}else if (outfit == 'b' || outfit == 'B'){
+    console.log(`Barbie: \"Ok! I will wear the leather leggings and jacket! I'll go get dressed and finish getting ready. I have to hurry! Ken will be here any minute.\"`);
+}else if (outfit == 'c' || outfit == 'C'){
+    console.log(`Barbie: \"Ok! I will wear the jeans and the blazer! I love this blazer. I'll go get dressed and finish getting ready. I have to hurry! Ken will be here any minute.\"`);
+}
+
+
 
 console.log("\nKEN ARRIVES. DING-DONG!\n");
 
@@ -57,29 +71,47 @@ console.log("KEN AND BARBIE HEAD OVER TO SOKAI, A JAPAENESE-PREUVIAN FUSION REST
 console.log("\nBarbie thinks to herself: \"Hmm... What should I have?\"");
 
 //QUESTION 2
-console.log("\nThe menu has a number of choices. Barbie is eyeing the sushi pizza, the beef tenderloin stir fry and the salmon tacos.\n\nWhich one do you think she should choose?\n A) Sushi Pizza\n B) Beef Tenderloin Stir Fry\n C) Salmon Tacos")
+console.log("\nThe menu has a number of choices. Barbie is eyeing the sushi pizza, the beef tenderloin stir fry and the salmon tacos.\n")
+
+let food = readlineSync.keyIn("\nWhich one do you think she should choose?\n a) Sushi Pizza\n b) Beef Tenderloin Stir Fry\n c) Salmon Tacos\n",  {limit: 'abcABC'});
 
 console.log("\nBARBIE AND KEN ARE CHATTING, KEN ASKS BARBIE TO BE HIS GIRLFRIEND\n");
 
-//QUESTION 2 - CONFIRMATION - ELSEIF
 
-console.log("Ken: I am stuffed! Did you like your BARBIES FOOD SELECTION?\"");
 
-console.log("Barbie: \"It was absolutely delicious\"");
+
+if(food == 'a' || food == 'A'){
+    console.log("Ken: I am stuffed! Did you like your sushi pizza?\"");
+}else if (food == 'b' || food == 'B'){
+    console.log("Ken: I am stuffed! Did you like your beef tenderloin?\"");
+}else if (food == 'c' || food == 'C'){
+    console.log("Ken: I am stuffed! Did you like your salmon tacos?\"");
+}
+
+
+
+
+console.log("\nBarbie: \"It was absolutely delicious!\"\n");
 
 console.log("Ken: \"Barbie, I have had a great time with you over these last few dates, I would really like to make you a part of my life. Will you be my girlfriend?\"");
 
 //QUESTION 3
 
-console.log("What should Barbie say?\n A) YES!\n B) I think its too soon Ken, is it ok if we get to know each other more?\n C) No, I was really just looking to have fun.\n")
+let gfAnswer = readlineSync.keyIn("What should Barbie say?\n a) YES!\n b) I think its too soon Ken, is it ok if we get to know each other more?\n c) No, I was really just looking to have fun.\n", {limit: 'abcABC'});
 
 console.log("DINNER ENDS, KEN DRIVES BARBIE HOME AND WALKS HER TO HER DOOR")
 
 
-//Scenarios: 
-//If Barbie says yes: Ken says "Barbie, I had so much fun tonight. See you tomorrow, girlfriend." and leans in for a kiss. 
-//If Barbie said it's too soon, Ken says "Barbie, I had so much fun tonight. I hope you did too. I'll call you tomorrow to setup another date?"
-//If Barbie said no, Ken says: "I undestand you were only looking for fun Barbie, but I am looking for a relationship, it might be best for us to just be friends, I'll see you around."
+
+if(gfAnswer == 'a' || gfAnswer == 'A'){
+    console.log("Ken says \"Barbie, I had so much fun tonight. See you tomorrow, girlfriend.\" and leans in for a kiss.");
+}else if (gfAnswer == 'b' || gfAnswer == 'B'){
+    console.log("Ken says \"Barbie, I had so much fun tonight. I hope you did too. I'll call you tomorrow to setup another date?\"");
+}else if (gfAnswer == 'c' || gfAnswer == 'C'){
+    console.log("Ken says: \"I undestand you were only looking for fun Barbie, but I am looking for a relationship, it might be best for us to just be friends, I'll see you around.\"");
+}
+
+
 
 
 console.log("Barbie: \"Thanks for helping me tonight my friend!\"")
